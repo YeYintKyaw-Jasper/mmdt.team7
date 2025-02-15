79e227911c2b793ebcd09c5799fe8576c4938c13
YeYintKyaw-Jasper <yeyint1912@gmail.com>
2025-02-15 23:05:05 +0700

    First commit

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+hr = 365*24
+print("Hours in a year : ",hr)
+min = 3650*24*60
+print("Minutes in a decade : ",min)
+s = 365*23*3600
+print("Seconds old in years : ",s)
+
+#exercise2
+old = 1406000000/s
+print("1406seconds in a year : ",hr)
---
+++ exercise5.py
@@ -0,0 +1,6 @@
+#Write a function triangle_area(b, h) that returns the area of a triangle. Call the function for b = 2.28 and h = 3.55. Print the result.
+
+def triangle_area(b,h):
+    a = b*h/2
+    return a
+print(triangle_area(2.28,3.55))
